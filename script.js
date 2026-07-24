543f328e09d0cfb1ec1763eb2684cd39f5900583
fake708121-lab <fake708121@gmail.com>
2026-07-25 02:02:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-document.getElementById("startBtn").onclick=function(){
+const text="A Special Surprise For Rakshu ❤️";
+
+let i=0;
+
+function type(){
+
+if(i<text.length){
+
+document.getElementById("typing").innerHTML+=text.charAt(i);
+
+i++;
+
+setTimeout(type,80);
+
+}
+
+}
+
+type();
+
+document.getElementById("startBtn").onclick=()=>{
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+
 window.location.href="pages/birthday.html";
+
+},800);
+
 }
